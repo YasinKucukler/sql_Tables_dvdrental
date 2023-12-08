f8ad3e48485eda32e213a6fe1a9b3c54e2e2c624
Yasin  KÜÇÜKLER <84202117+YasinKucukler@users.noreply.github.com>
2023-12-08 13:35:05 +0300

    Add files via upload
    8th commit create - update - delete table

---
+++ 8/8.sql
@@ -0,0 +1,17 @@
+--CREATE TABLE employee(
+--	id INTEGER primary key,
+--	name VARCHAR(50),
+--	birthday DATE,
+--	email VARCHAR(100)
+--);
+select * from employee
+
+--UPDATE 	employee
+--SET name = 'XXXXX'
+--WHERE id BETWEEN 5 AND 10;
+
+--DELETE FROM employee 
+--WHERE id IN (2,3, 11, 22,23)
+--RETURNING *;
+
+
